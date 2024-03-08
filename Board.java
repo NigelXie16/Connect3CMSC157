2bca894201a1308e3af255a0e5e465145046d168
zaratesantiago03 <162635132+zaratesantiago03@users.noreply.github.com>
2024-03-07 19:46:31 -0500

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,10 +1,14 @@
-class Board(){
+class Board()
+//instance variables 
+{
 int len;
 Piece p;
 int col;
 Piece [][] B;
 
-public Board(){
+public Board()
+//constructor, initially each spot set to null
+{
     len=5;
     for(int i=0; i<len; i++){
         for(int j=0; j<len; j++){
@@ -12,11 +16,27 @@ public Board(){
         }
     }
 }
-public Board( int col){
+public Board( int col)
+//constructor 
+{
+this.len=col;
+}
+public int getSize()
+//return board size 
+{
+return len;
+}
+public String toString()
+//return the board String to visible set of columns 
+{
+
+
+
 
 }
-
-public add(Piece P, int col) {
+public add(Piece P, int col)
+//add piece to a column     
+{
     while(int j=0; j<len; ){
         if B[col][j]
     }
