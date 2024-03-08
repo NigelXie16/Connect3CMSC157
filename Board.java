155dfec0ba7e03894a8d4029d30f040fef9e239a
zaratesantiago03 <162635132+zaratesantiago03@users.noreply.github.com>
2024-03-07 19:31:24 -0500

    Create Board.java

---
+++ Board.java
@@ -0,0 +1,31 @@
+class Board(){
+int len;
+Piece p;
+int col;
+Piece [][] B;
+
+public Board(){
+    len=5;
+    for(int i=0; i<len; i++){
+        for(int j=0; j<len; j++){
+            B[i][j]=null;
+        }
+    }
+}
+public Board( int col){
+
+}
+
+public add(Piece P, int col) {
+    while(int j=0; j<len; ){
+        if B[col][j]
+    }
+}
+
+
+
+
+
+
+
+}
