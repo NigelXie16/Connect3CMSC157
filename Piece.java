03182c747938800ee6be4107d2642b3357157ea5
Nigel Xie <43504628+NigelXie16@users.noreply.github.com>
2024-03-14 16:05:12 -0400

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -1,15 +1,15 @@
-public class Piece {
-    public static char PLAYER1 = 'X';
-    public static char PLAYER2 = 'O';
+ class Piece {
+    public static char player = 'X';
+    //public static char PLAYER2 = 'O'; //maybe only 
 
     // Constructor to initialize the piece with a specified player
-public Piece(char player) {
-    this.player = player;
+    public Piece(char player) {
+        this.player = player;
     }
 
-    // Method to represent the piece as a string 
+    // Method to represent the piece as a string
     @Override
-public String toString() {
-    return String.valueOf(player);
+    public String toString() {
+        return String.valueOf(player);
     }
 }
