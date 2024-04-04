f044cbe0e4aeda7bba5d9c3341727307e785e8b8
zaratesantiago03 <162635132+zaratesantiago03@users.noreply.github.com>
2024-04-04 16:59:33 -0400

    Update TokenVis.java
    added comments interface

--- TokenVis.java
+++ TokenVis.java
@@ -2,16 +2,16 @@ import java.awt.*;
 
 public class TokenVis extends Rectangle{
     private Color color;
-
+/**this is the constructor of the TokenVis, parameter expecting 
     public TokenVis(int x0, int y0, int w, int h) {
         super(x0, y0, w, h);
     }
-    
+   /** we will draw the token here*/ 
     public void draw(Graphics g) {
         g.setColor(color);
         g.fillOval(x, y, width, height);
     }
-
+/**we will set the player color with parameter of player (player1 or player 2)*/
     public void setColor(Color player) {
         this.color = player;
     }
